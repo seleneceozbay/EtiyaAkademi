9b1561d9d10df32345facf79ca9528dd5b6b161f
seleneceozbay <s.eceozbay@gmail.com>
2023-04-05 20:42:04 +0300

    added

--- homework1.py
+++ homework1.py
@@ -28,15 +28,80 @@ print(sonucMesaj)
 # ŞART BLOĞU -START
 
 dolarDun = 7.65
-dolarBugun = 7.35
+dolarBugun = 7.85
 if dolarDun > dolarBugun:
     print("Azalış oku")
     print("Bitti")
 
-if dolarDun < dolarBugun:
+elif dolarDun < dolarBugun:
     print("Artış oku")
 
-if dolarDun == dolarBugun:
+else:
     print("Eşittir oku")
 
+print("Bitti")
+
+
+# if dolarDun < dolarBugun:
+#     print("Artış oku")
+
+# if dolarDun == dolarBugun:
+#     print("Eşittir oku")
+
 # ŞART BLOĞU -END
+
+# ARRAY -START
+
+krediler = ["Hızlı Kredi", "Maaş Kredisi", "Emekli İhtiyaç Kredisi"]
+
+print(krediler)
+print(krediler[0])
+print(krediler[1])
+print(krediler[2])
+
+print(len(krediler))
+
+krediler[0] = "Çabuk Kredi"
+print(krediler)
+
+# ARRAY -END
+
+# DÖNGÜLER -START
+
+for kredi in krediler:
+    print(kredi)
+
+# alias => döngü sırasında bulunduğu değer
+
+for i in range(10):
+    print(i)
+
+for i in range(len(krediler)):
+    print(krediler[i])
+
+for i in range(3, 10):
+    print(i)
+
+for i in range(0, 10, 2):
+    print(i)
+
+# DÖNGÜLER -END
+
+# FONKSİYONLAR -START
+
+print("--İLK SAYFA")
+
+
+def kredileriListele():
+
+    krediler = ["Hızlı Kredi", "Maaş Kredisi", "Emekli İhtiyaç Kredisi"]
+
+    for kredi in krediler:
+        print(kredi)
+
+
+kredileriListele()
+
+print("--İKİNCİ SAYFA")
+kredileriListele()
+# FONKSİYONLAR -END
